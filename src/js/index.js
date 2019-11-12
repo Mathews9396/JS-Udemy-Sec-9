dfb5797f726906378ade9da2c1cf78242960cf5c
Mathews9396 <49215917+Mathews9396@users.noreply.github.com>
2019-11-12 09:33:15 +0530

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+// Global app controller
+
+import x from './test';
+
+console.log(`Changing and logging the number ${x} from test.js file`);
---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+console.log('this is the test file talking');
+export default 12345;
